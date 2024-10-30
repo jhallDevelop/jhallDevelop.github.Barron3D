21684d503615f22a0db84728a4158b7f227953ee
jhallDevelop <jhall.develop@gmail.com>
2024-10-31 08:55:18 +1000

    ADDED: new build with some bug fixes

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Tully360-0.1";
 const contentToCache = [
-    "Build/Build_DG.loader.js",
-    "Build/Build_DG.framework.js",
-    "Build/Build_DG.data",
-    "Build/Build_DG.wasm",
+    "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
+    "Build/950b0388c18dd6352d24ca62ffe6cedb.framework.js",
+    "Build/0e5b8f2923c47e6c349d8b9c212500b5.data",
+    "Build/5d788d243bdced2cfbb9d8523f01867e.wasm",
     "TemplateData/style.css"
 
 ];
