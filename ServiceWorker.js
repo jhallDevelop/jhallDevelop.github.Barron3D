7564383d7019cbc5ddef6020a391a2c9ea856c13
joshua@enabler.com <jhall.develop@gmail.com>
2024-12-03 11:43:37 +1000

    EDIT: update with content changes

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Tully360-0.1";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/8fbfef998962cb28ce7dff134a12a818.framework.js",
-    "Build/549ef78259209a0c4e0526d9287ace2e.data",
-    "Build/148a8585beaf70087a25db2609bf8a1e.wasm",
+    "Build/34ae7464d2d96225d34353f151a3f7e1.framework.js",
+    "Build/986207f157a38c65c335a0776d51ebd5.data",
+    "Build/734f03667ecd0213b533daf5672a44b8.wasm",
     "TemplateData/style.css"
 
 ];
